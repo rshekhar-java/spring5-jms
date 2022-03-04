744f58e7520a02fe99048491989ad6b3bb84046d
Ravi Shekhar <rsshekhar27@outlook.com>
2022-03-04 09:59:56 -0600

    Adding Pojo

---
+++ src/main/java/com/rs/springframework/model/HelloWorldMessage.java
@@ -0,0 +1,23 @@
+package com.rs.springframework.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+/**
+ * created by rs 3/3/2022.
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloWorldMessage {
+    static final long serialVersionUID = -6703826490277916847L;
+
+    private UUID id;
+    private String message;
+
+}
