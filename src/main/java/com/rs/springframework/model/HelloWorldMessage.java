78a93f40b9e003e89a2c07d88fa72d442ac5f00b
Ravi Shekhar <rsshekhar27@outlook.com>
2022-03-04 10:42:39 -0600

    Added Embedded server for ActivMQ

--- src/main/java/com/rs/springframework/model/HelloWorldMessage.java
+++ src/main/java/com/rs/springframework/model/HelloWorldMessage.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.util.UUID;
 
 /**
@@ -14,7 +15,7 @@ import java.util.UUID;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class HelloWorldMessage {
+public class HelloWorldMessage implements Serializable {
     static final long serialVersionUID = -6703826490277916847L;
 
     private UUID id;
