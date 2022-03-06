6e7d1ad27661a2c38bf0d27a3f1745ac74c9043a
Ravi Shekhar <rsshekhar27@outlook.com>
2022-03-05 18:32:20 -0600

    Added JmsConfig

---
+++ src/main/java/com/rs/springframework/config/JmsConfig.java
@@ -0,0 +1,22 @@
+package com.rs.springframework.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+/**
+ * created by rs 3/5/2022.
+ */
+@Configuration
+public class JmsConfig {
+    @Bean
+    public MappingJackson2MessageConverter messageConverter(){
+
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        return converter;
+    }
+
+}
