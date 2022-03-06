d47df76bcff9b01bee1567a423e397d86aba70d8
Ravi Shekhar <rsshekhar27@outlook.com>
2022-03-05 18:40:01 -0600

    Added sender message

--- src/main/java/com/rs/springframework/config/JmsConfig.java
+++ src/main/java/com/rs/springframework/config/JmsConfig.java
@@ -10,6 +10,8 @@ import org.springframework.jms.support.converter.MessageType;
  */
 @Configuration
 public class JmsConfig {
+    public static final String MY_QUEUE = "my-hello-world";
+
     @Bean
     public MappingJackson2MessageConverter messageConverter(){
 
---
+++ src/main/java/com/rs/springframework/sender/HelloSender.java
@@ -0,0 +1,34 @@
+package com.rs.springframework.sender;
+
+import com.rs.springframework.config.JmsConfig;
+import com.rs.springframework.model.HelloWorldMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+/**
+ * created by rs 3/5/2022.
+ */
+@RequiredArgsConstructor
+@Component
+public class HelloSender {
+private final JmsTemplate jmsTemplate;
+
+@Scheduled(fixedRate = 2000)
+public void sendMessage(){
+    System.out.println("I am Sending a message");
+
+    HelloWorldMessage message = HelloWorldMessage
+            .builder()
+            .id(UUID.randomUUID())
+            .message("Hello World !!")
+            .build();
+     jmsTemplate.convertAndSend(JmsConfig.MY_QUEUE,message);
+
+     System.out.println("Message Sent Successfully!");
+}
+
+}
